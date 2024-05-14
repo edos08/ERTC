72d6ed28362e038f530a50fc8a390885520599d5
Edoardo Scarpel <edoardo.scarpel@gmail.com>
2024-05-14 12:15:27 +0200

    LAB 3 continues and LAB 4 started

--- LAB3/Core/Src/stm32f7xx_hal_msp.c
+++ LAB3/Core/Src/stm32f7xx_hal_msp.c
@@ -492,6 +492,9 @@ void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* htim_encoder)
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
+    /* TIM4 interrupt Init */
+    HAL_NVIC_SetPriority(TIM4_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(TIM4_IRQn);
   /* USER CODE BEGIN TIM4_MspInit 1 */
 
   /* USER CODE END TIM4_MspInit 1 */
@@ -755,6 +758,8 @@ void HAL_TIM_Encoder_MspDeInit(TIM_HandleTypeDef* htim_encoder)
     */
     HAL_GPIO_DeInit(GPIOD, TIM4_CH1_ENC2A_Pin|TIM4_CH2_ENC2B_Pin);
 
+    /* TIM4 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(TIM4_IRQn);
   /* USER CODE BEGIN TIM4_MspDeInit 1 */
 
   /* USER CODE END TIM4_MspDeInit 1 */
